01f240c3541e3858998b74243c9ea0514aa0525c
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-11 12:16:14 +0200

    get Genres from backend

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,13 +1,12 @@
-import Raven from "raven-js";
-
 function init() {
-  Raven.config(
+  /*   Raven.config(
     "https://0a1f230d026a4924a6bee2c3ef401b73@o1283220.ingest.sentry.io/6492714"
-  ).install();
+  ).install(); */
 }
 
 function log(error) {
-  Raven.captureException(error);
+  /*   Raven.captureException(error); */
+  console.log(error);
 }
 
 export default {
