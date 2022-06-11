18b2f5491f219d880668509f016a9aa8879257f3
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-11 13:34:57 +0200

    refactoring

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,25 +1,28 @@
-import { getGenres } from "./genreService";
 import http from "./httpService";
 import { apiMovieEndpoint } from "../config.json";
+
+function getMovieUrl(id) {
+  return `${apiMovieEndpoint}/${id}`;
+}
 
 export function getMovies() {
   return http.get(apiMovieEndpoint);
 }
 
 export function getMovie(id) {
-  return http.get(apiMovieEndpoint + "/" + id);
+  return http.get(getMovieUrl(id));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiMovieEndpoint + "/" + movie._id, body);
+    return http.put(getMovieUrl(movie._id), body);
   }
 
   return http.post(apiMovieEndpoint, movie);
 }
 
 export function deleteMovie(id) {
-  http.delete(apiMovieEndpoint + "/" + id);
+  http.delete(getMovieUrl(id));
 }
