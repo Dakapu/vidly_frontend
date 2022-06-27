73631ac03850aebcc34ffcd86130924e9ce01384
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-27 10:38:30 +0200

    fix typo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-console.log("Process: ", process.env);
+console.log("Process Name: ", process.env);
 
 ReactDOM.render(
   <BrowserRouter>
